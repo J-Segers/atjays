c84da9266b3edd78fa83cc7eb4829b788960cb22
J-Segers <jethroseger@gmail.com>
2023-04-13 18:20:11 +0200

    main change: added routing using react-router-dom v6
    extra: changed projecten folder to projects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Home from './pages/home';
+import About from './pages/About';
+import Projects from './pages/projects';
+import Footer from './components/footer/Footer';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div id="app-container">
+      <Header />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/aboutme"} element={<About />} />
+          <Route path={"/projects"} element={<Projects />} />
+        </Routes>
+      <Footer />
+    </div>
   );
 }
 
