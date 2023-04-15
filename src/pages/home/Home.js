016294ee744feff74033ad180706d9f2683b351b
J-Segers <jethroseger@gmail.com>
2023-04-15 13:32:53 +0200

    home page basic style is done still have to fix media query's added cartoon to assets

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import "./Home.css";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import cartoon from "../../assets/atjays_cartoon.png";
 
 function Home() {
     return (
-        <main>
-            
-        </main>
+        <div id="home-container">
+            <Header />
+            <main>
+                <section id="hero">
+                    <article>
+                        <h2>
+                            Fullstack<br />Developer
+                        </h2>
+                        <p className={"hero-text"}>
+                            I'm Jethro a fullstack developer from the Netherlands. I love learning new techniques so I can make better and more expressive websites.
+                        </p>
+                    </article>
+                    <img src={cartoon} alt="" />
+                </section>
+                <section className="projects">
+                    &nbsp;
+                </section>
+            </main>
+            <Footer />
+        </div>
     );
 }
 export default Home;
