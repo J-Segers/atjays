2c52416c69c17aec61257c99e0a47d1e68fd1f52
J-Segers <jethroseger@gmail.com>
2023-04-15 13:29:12 +0200

    removed header and footer components moved contact styling to Footer.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Projects from './pages/projects/Projects';
-import Footer from './components/footer/Footer';
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div id="app-container">
-      <Header />
         <Routes>
-          <Route path={"/"} element={<Home />} />
+          <Route exact path={"/"} element={<Home />} />
           <Route path={"/aboutme"} element={<About />} />
           <Route path={"/projects"} element={<Projects />} />
         </Routes>
-      <Footer />
     </div>
   );
 }
