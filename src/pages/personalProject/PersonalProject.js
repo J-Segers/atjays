f9682389e2ac99d16720af132ca83f7da1ac4908
J-Segers <jethroseger@gmail.com>
2023-05-01 00:46:31 +0200

    Personal projects page added. This will embed my personal projects from my github repositories

---
+++ src/pages/personalProject/PersonalProject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./PersonalProject.css";
+
+function PersonalProject() {
+    return (
+        <div className="personal-container">
+            <embed src="https://j-segers.github.io/teashop/" />
+        </div>
+    );
+}
+
+export default PersonalProject;
