187a60b89b1fc876b032c337d82cdc13c8a871c4
J-Segers <jethroseger@gmail.com>
2023-06-06 11:18:23 +0200

    started on projects page fixed conflicting css between pages home and projects

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,7 +9,7 @@ function Home() {
     return (
         <div id="home-container">
             <Header />
-            <main>
+            <main id='main-home'>
                 <section id="hero">
                     <article>
                         <h2>
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import "./Projects.css";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+import Project from "../../components/project/Project"
 
 function Projects() {
     return (
-        <main>
-            
-        </main>
+        <>
+            <Header />
+            <main id={"main-projects"}>
+                <Project src={"https://j-segers.github.io/teashop/"}/>
+                <Project src={"https://j-segers.github.io/teashop/"}/>
+            </main>
+            <Footer />
+        </>
+       
     );
 }
 
