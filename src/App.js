2a4716e2fc51576515bf654a26cf27d3bfe88126
J-Segers <jethroseger@gmail.com>
2023-09-05 13:31:39 +0200

    fixed height issues

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import PersonalProject from './pages/personalProject/PersonalProject';
 function App() {
   return (
     <div id="app-container">
-        <Routes>
-          <Route exact path={"/"} element={<Home />} />
-          <Route path={"/aboutme"} element={<About />} />
-          <Route path={"/projects"} element={<Projects />} />
-          <Route path={"/personal-project"} element={<PersonalProject />} />
-        </Routes>
+      <Routes>
+        <Route exact path={"/"} element={<Home />} />
+        <Route path={"/aboutme"} element={<About />} />
+        <Route path={"/projects"} element={<Projects />} />
+        <Route path={"/personal-project"} element={<PersonalProject />} />
+      </Routes>
     </div>
   );
 }
